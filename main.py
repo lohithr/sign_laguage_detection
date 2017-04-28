fad6f98e6c345421feae4e7d1ce685355af8aa03
thanujraju <p.thanuj97@gmail.com>
2017-04-28 18:43:29 +0530

    resampling is done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import scipy.signal		
 #import pandas as pd 
 import glob
 mymap =  {}
@@ -14,40 +15,33 @@ traind_len = len(data_sets)
 counter = 0
 val = 0
 for i in range(traind_len):
-	
 	obs_files = glob.glob(data_sets[i]+"/*")
 	for j in range(len(obs_files)):
-		data =  open(obs_files[j]).read().split()
-		data = [float(l) for l in data]
-		#temp = obs_files[j]
+		file =  open(obs_files[j])
+		#converting attribute data to a 2D array
+		data = [[float(n) for n in line.split()] for line in file]
+		#resampling the data to sample 57 frames which is an average no of frames for all words
+		data = scipy.signal.resample(data,57)
+		#converting the list to an arry ang flattering  to form a 1d arry of attribute values 
+		data = np.array(data)
+		data = np.ndarray.flatten(data)
+		#extracting the word from the file name 
 		obs_files[j] = obs_files[j].replace(data_sets[i]+"/","")
 		for j1 in range(6):
 			obs_files[j] = obs_files[j].replace("-"+str(j1+1)+".tsd","")
 			obs_files[j] = obs_files[j].replace("_"+str(j1),"")
-		#print temp,obs_files[j]
+		#Each word is mapped to a distinct integer to represent a class
 		if obs_files[j] not in mymap:
 			mymap[obs_files[j]] = counter
 			counter = counter +1
-		val = max(val,len(data))
+		# tcdodd folders has data  to train and test folder has test data so based on file location appending file data into 
+		# corresponding arrays
 		if "tctodd" in data_sets[i]:
 			train.append(data)
 			res.append(mymap[obs_files[j]])
 		if "test" in data_sets[i]:
 			test.append(data)
 			test_res.append(mymap[obs_files[j]])
-
-i = 0
-for i in range(len(train)):
-	j=0
-	for j in range(val-len(train[i])):
-		train[i].append(float(0))
-i = 0
-for i in range(len(test)):
-	j=0
-	for j in range(val-len(test[i])):
-		test[i].append(float(0))
-	#print train[i]
-
 
 ########################################################
 from sklearn.linear_model import SGDClassifier
